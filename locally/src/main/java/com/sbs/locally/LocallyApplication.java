0d7b4cca2f6790ce0ba62dabf65aac19e75e6c61
seo <seo@192.168.219.106>
2025-04-23 16:38:45 +0900

    2024/04/23
    locally 첫 업로드

---
+++ locally/src/main/java/com/sbs/locally/LocallyApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.locally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocallyApplication.class, args);
+	}
+
+}
---
+++ locally/src/test/java/com/sbs/locally/LocallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.locally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
